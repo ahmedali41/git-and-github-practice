638d08e7171bc095921e7581c0a9e6a84c11a87d
ahmedali41 <aahmedali4123@gmail.com>
2026-02-14 12:42:43 +0500

    add new function

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log("hello world");
 const fruits = ["Mango", "Apple", "Orange"];
 
 console.log(fruits);
+
+const student = {
+  name: "Ahmed Ali",
+  age: 23,
+  city: "Lahore",
+  adult: true,
+};
+
+console.log(student);
